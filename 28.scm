c84d2bf3bf95d806984ba77c5918075d62c73deb
Fengyuan Chen <cfy1990@gmail.com>
2011-02-28 11:08:18 +0800

    csc 28.scm && ./28 or guile 28.scm

---
+++ 28.scm
@@ -0,0 +1,9 @@
+(define *+
+  (lambda (a b c)
+    (+ (* a b) c)
+    ))
+(define pe-28
+  (lambda (r)
+    (define n (/ (- r 1) 2))
+    (/  (*+ n (*+ n (*+ 16 n 30) 26) 3) 3)))
+(display (pe-28 1001))
